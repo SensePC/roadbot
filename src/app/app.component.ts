d498f8c9e65af84c8e8baa6b8d98a85dc43b428c
gpetrak <gkpetrak@gmail.com>
2018-06-11 00:03:04 +0300

    adding a menu bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, MenuController, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -8,15 +8,40 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+  
+  // make HomePage the root (of first) page
   rootPage:any = HomePage;
+  pages: Array<{title: string, component: any}>;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(public platform: Platform,
+    public menu: MenuControler, 
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen
+    ) {
+    this.initializeApp();
+
+    //set our app's pages
+     this.pages = [
+      { title: 'Report', component: home },
+      { title: 'Emergency', component: emerg }
+    ];
+  }
+
+  initializeApp() {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
   }
 }
 
--- src/pages/emerg/emerg.ts
+++ src/pages/emerg/emerg.ts
@@ -1,25 +1,99 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the EmergPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Http } from '@angular/http';
+ 
+declare var google;
+ 
 @Component({
-  selector: 'page-emerg',
-  templateUrl: 'emerg.html',
+  selector: 'emerg-page',
+  templateUrl: 'emerg.html'
 })
-export class EmergPage {
+export class HomePage {
+  data:any = {};
+ 
+  @ViewChild('map') mapElement: ElementRef;
+  map:emerg;
+ 
+  constructor(public navCtrl: NavController, public geolocation: Geolocation, public http: Http) {
+    this.data.desc = '';
+    this.data.response = '';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.http = http;
+ 
+  }  
+
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+ 
+  loadMap(){
+ 
+    this.geolocation.getCurrentPosition().then((position) => {
+ 
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+      let mapOptions = {
+        center: latLng,
+        zoom: 18,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+ 
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+    }, (err) => {
+      console.log(err);
+    });
+ 
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EmergPage');
+  addMarker(){
+ 
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+ 
+    let content = "<h4>Information!</h4>";         
+ 
+    this.addInfoWindow(marker, content);
+ 
+    }   
+ 
+  addInfoWindow(marker, content) {
+    
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
   }
+  
+  submit() {
+    this.geolocation.getCurrentPosition().then((position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+	let latLng = {lat: lat, lon:lon}
+    
+    var title = this.data.title;
+    var desc = this.data.desc;
+    var desc_loc = {lat: lat, lon: lon, title: title, desc: desc}
+    var link = 'http://192.168.1.6:8000/results/';
+    var myData = JSON.stringify(desc_loc);
+ 
+ 
+    this.http.post(link, myData)
+      .subscribe(data => {
+      this.data.response = data["_body"]; 
+      }, error => {
+      console.log("Oooops!");
+    });
+  });
+ }
 
 }
+
+
