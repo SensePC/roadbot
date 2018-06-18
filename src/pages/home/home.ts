f2786471ca4fe7077bcca4aa657ab88f746a6954
gpetrak <gkpetrak@gmail.com>
2018-06-18 16:19:22 +0300

    adding a leaflet map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LocationTracker } from '../../providers/location-tracker';
- 
+import leaflet from 'leaflet';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  @ViewChild('map') mapContainer: ElementRef;
+  map: any;
  
   constructor(public navCtrl: NavController, public locationTracker: LocationTracker) {
  
   }
+
+  ionViewDidEnter() {
+    this.loadmap();
+  }
+
+  loadmap() {
+    this.map = leaflet.map("map").fitWorld();
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'www.tphangout.com',
+      maxZoom: 18
+    }).addTo(this.map);
+    this.map.locate({
+      setView: true,
+      maxZoom: 15
+    }).on('locationFound', (e) => {
+       // let markerGroup = leaflet.featureGroup();
+        let marker: any = leaflet.marker([e.latitude, e.longitude]);
+        this.map.addLayer(marker);
+     // markerGroup.addLayer(marker);
+     //this.map.addLayer(markerGroup);
+     }).on('locationerror', (err) => {
+       alert(err.message);
+    })
+  } 
  
   start(){
     this.locationTracker.startTracking();
@@ -21,3 +49,5 @@ export class HomePage {
   }
  
 }
+
+
