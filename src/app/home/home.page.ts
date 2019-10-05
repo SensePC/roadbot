891426972e39d71c7db20fb345c1ccd7e288cad3
gpetrak <gkpetrak@gmail.com>
2019-10-05 13:13:28 +0300

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  geoLatitude: number;
+  geoLongitude: number;
+
+  watchLocationUpdates:any;
+  isWatching:boolean;
+
+  constructor(private geolocation: Geolocation) {}
+
+  //Get current coordinates of device
+  getGeolocation(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.geoLatitude = resp.coords.latitude;
+      this.geoLongitude = resp.coords.longitude; 
+     }).catch((error) => {
+       alert('Error getting location'+ JSON.stringify(error));
+     });
+  }
+
+ //Start location update watch
+ watchLocation(){
+  this.isWatching = true;
+  this.watchLocationUpdates = this.geolocation.watchPosition();
+  this.watchLocationUpdates.subscribe((resp) => {
+    this.geoLatitude = resp.coords.latitude;
+    this.geoLongitude = resp.coords.longitude; 
+  });
+}
+
+//Stop location update watch
+stopLocationWatch(){
+  this.isWatching = false;
+  this.watchLocationUpdates.unsubscribe();
+}
 
 }
