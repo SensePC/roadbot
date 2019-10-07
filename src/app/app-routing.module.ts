3f73d6d1752735a2c559b1b274f60878ee664af2
gpetrak <gkpetrak@gmail.com>
2019-10-07 14:13:34 +0300

    adding more pages and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'map', loadChildren: './map/map.module#MapPageModule' },
+  { path: 'form', loadChildren: './form/form.module#FormPageModule' },
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,25 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'Map',
+      url: '/map',
+      icon: 'map'
+    },
+    {
+      title: 'Form',
+      url: '/form',
+      icon: 'form'
+    }
+  ];
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
